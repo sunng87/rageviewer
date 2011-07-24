3284f1367fc7f982da727aaa0d7f8720ec65bda9
Sun Ning <classicning@gmail.com>
2011-07-24 11:34:32 +0800

    ui improve

--- resources/public/scripts/rageviewer.cljs
+++ resources/public/scripts/rageviewer.cljs
@@ -4,16 +4,30 @@
 (def current-rage-index -1)
 (defn by-id [id]
   (.getElementById (js* "document") id))
-
-(defn ^:export show-rage [rage-index]
+(defn js-date [arg]
+  (new (js* "Date") arg))
+ 
+(defn timestamp-to-date [ts]
+  (. (js-date (* 1000 ts)) (toUTCString)))
+    
+(defn to-imgur-url [url]
+  (or 
+    (first (re-matches #"http://.*?(png|jpg)$" url))
+    (str url ".png")))
+    
+(defn show-rage [rage-index]
   (let [rage (nth loaded-rages rage-index)]
+    (set! (.src (by-id "rage-img")) "")
+    (set! (.alt (by-id "rage-img")) "loading...") 
     (set! (.innerHTML (by-id "rage-title")) (aget rage "title"))
     (set! (.innerHTML (by-id "rage-voteup")) (aget rage "ups"))
     (set! (.innerHTML (by-id "rage-votedown")) (aget rage "downs"))
+    (set! (.title (by-id "rage-img")) (aget rage "title"))
+    (set! (.innerHTML (by-id "rage-author")) (aget rage "author"))
+    (set! (.innerHTML (by-id "rage-date")) 
+      (timestamp-to-date (aget rage "created")))
     (set! (.src (by-id "rage-img")) 
-              (or 
-                (first (re-matches #"http://.*?(png|jpg)$" (aget rage "url")))
-                (str (aget rage "url") ".png")))))
+      (to-imgur-url (aget rage "url")))))
 
 (defn ^:export show-next-rage []
   (if (< current-rage-index (count loaded-rages))
