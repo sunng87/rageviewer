5c5b94ef610e2de3c3be8b3824eb66b53dbd06a9
Sun Ning <classicning@gmail.com>
2011-07-27 21:04:04 +0800

    add lein-control

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.5"]
                  [reddit.clj "0.1.1"]]
-  :dev-dependencies [[lein-ring "0.4.5"]]
+  :dev-dependencies [[lein-ring "0.4.5"]
+                                   [lein-control "0.1.0"]]
   :ring {:handler rageviewer.core/app
          :init  rageviewer.core/app-init})
