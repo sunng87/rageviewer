3218b709af120e7f73bd84f42494ae4c82ec3340
Sun Ning <classicning@gmail.com>
2012-02-11 10:43:33 +0800

    upgrade reddit.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject rageviewer "1.0.0-SNAPSHOT"
   :description "A rage comic viewer"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]
+                 [org.clojure/data.json "0.1.2"]
                  [org.clojure/tools.logging "0.2.3"]
                  [compojure "1.0.1"]
-                 [reddit.clj "0.3.1"]
+                 [reddit.clj "0.3.3"]
                  [clj-redis "0.0.12"]]
   :dev-dependencies [[lein-ring "0.5.4"]
                      [lein-cljsbuild "0.0.12"]]
